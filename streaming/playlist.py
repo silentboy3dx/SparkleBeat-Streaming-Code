c002d36934be50576bed71774cbbe912e253aff4
Silentboy <118685262+silentboy3dx@users.noreply.github.com>
2024-10-13 14:29:38 +0200

    update

--- streaming/playlist.py
+++ streaming/playlist.py
@@ -175,6 +175,8 @@ class Playlist:
 
         :return: Song
         """
+        print("current_index", self.current_index)
+        print("songs_array", self.songs_array)
         return self.songs_array[self.current_index]
 
     def get_next_song(self) -> Song:
